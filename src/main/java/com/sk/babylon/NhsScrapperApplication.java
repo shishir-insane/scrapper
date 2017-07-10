cd9eeb7d87a38030d24401a43a502ae390d4c4aa
Shishir Kumar <shishir.insane@gmail.com>
2017-07-10 01:49:40 +0100

    Initial commit
    Initial commit

---
+++ src/main/java/com/sk/babylon/NhsScrapperApplication.java
@@ -0,0 +1,12 @@
+package com.sk.babylon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhsScrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NhsScrapperApplication.class, args);
+	}
+}
